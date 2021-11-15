23f48c21f957ee82371c4651cda7c700bbc2876e
max anselmo <itmanselmo@gmail.com>
2021-11-15 16:51:14 -0500

    added itemcard and minor details

--- react-front-end/src/components/CollectionCard.js
+++ react-front-end/src/components/CollectionCard.js
@@ -1,11 +1,18 @@
-import Button from "@mui/material"
+import {Button} from "@mui/material"
 
-const CollectionCard = ({ collection }) => {
+const CollectionCard = ({ collection, setShowDetail }) => {
 	// onclick, takes you to CollectionDetail
+
+	const totalValue = () => {
+		let sum = 0;
+		collection.items.forEach(item => sum += item.price)
+		return sum
+	}
+
 	return <div className="collection-card">
-		<h3>${collection.name}</h3>
-		<p>Total Items: ${collection.items.length}</p>
-		<p>Total Value: summed item values</p>
+		<h3>{collection.name}</h3>
+		<p>Total Items: {collection.items.length}</p>
+		<p>Total Value: ${totalValue()}</p>
 		<Button>View More</Button>
 	</div>;
 };
--- react-front-end/src/components/CollectionDetail.js
+++ react-front-end/src/components/CollectionDetail.js
@@ -1,5 +1,25 @@
+import { Button } from "@mui/material";
+// import { DeleteIcon } from "@mui/icons-material/Delete";
+import ItemCard from "./ItemCard";
+
 const CollectionDetail = ({ collection }) => {
-	return <div></div>;
+
+	return <div className="collection-detail">
+		<Button>Return To Hub</Button>
+		<Button>Manage Collection</Button>
+		<Button color="error" >Delete Collection</Button>
+
+		<div className="collection-overview">
+			<h3>Overview</h3>
+			<p>Total Items: {collection.items.length}</p>
+			<p>Total Value: total value</p>
+		</div>
+
+		<div className="collection-items">
+			{collection.items.map(item => <ItemCard item={item} />)}
+		</div>
+		
+	</div>;
 };
 
 export default CollectionDetail;
